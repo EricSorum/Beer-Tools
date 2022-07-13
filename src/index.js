9ef127ce76381c19054eec2a1bfbc9db1bce9172
Eric Sorum <ejsorum@gmail.com>
2022-07-13 09:14:37 -0500

    Added style and button functionality.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
+import julianFunc from './julian.js'
+import abvConverterFunc from './abvconverter.js'
+import volumeFunc from './volume.js'
+
 const content = document.getElementById("content")
-const el = document.createElement("button")
-el.innerHTML = "Tab number one"
-content.appendChild(el)
-console.log("index.js")
+
+const heading = document.createElement("h1")
+heading.innerText = "Beer Tools"
+content.appendChild(heading)
+
+const buttonBox = document.createElement("div")
+buttonBox.id = "buttonBox"
+content.appendChild(buttonBox)
+
+const toolBox = document.createElement("div")
+content.appendChild(toolBox)
+toolBox.id = "toolBox"
+
+const startingText = document.createElement("div")
+startingText.innerHTML = "Select a Beer Tool above."
+toolBox.appendChild(startingText)
+
+const julianButton = document.createElement("button")
+const abvButton = document.createElement("button")
+const volumeButton = document.createElement("button")
+julianButton.innerText = "Julian Date Converter"
+abvButton.innerText = "ABV Calculator"
+volumeButton.innerText = "Volume Calculator"
+
+buttonBox.appendChild(julianButton)
+buttonBox.appendChild(abvButton)
+buttonBox.appendChild(volumeButton)
+
+julianButton.addEventListener("click", julianFunc)
+abvButton.addEventListener("click", abvConverterFunc)
+volumeButton.addEventListener("click", volumeFunc)
+
+
---
+++ src/volume.js
@@ -0,0 +1,44 @@
+export default function volumeFunc() {
+   const content = document.getElementById("content")
+
+   const topBox = document.createElement("div")
+   
+   const toolBox = document.getElementById("toolBox")
+   toolBox.innerHTML = []
+
+   const volumeLabel = document.createElement("label")
+   volumeLabel.innerText = 'Volume per can or bottle: '
+   const volumeForm = document.createElement("input")
+   const countLabel = document.createElement("label")
+   countLabel.innerText = 'Number of cans or bottles per pack: '
+   const countForm = document.createElement("input")
+   const costLabel = document.createElement("label")
+   costLabel.innerText = 'Cost of each pack: '
+   const costForm = document.createElement("input")
+      
+
+   const opButton = document.createElement("button")
+   opButton.innerText = "Submit"
+   opButton.addEventListener("click", volumeConverter)
+
+   const answerBox = document.createElement("div")
+
+   toolBox.appendChild(volumeLabel)
+   toolBox.appendChild(volumeForm)
+   toolBox.appendChild(countLabel)
+   toolBox.appendChild(countForm)
+   toolBox.appendChild(costLabel)
+   toolBox.appendChild(costForm)
+   toolBox.appendChild(opButton)
+   toolBox.appendChild(answerBox)
+
+   function volumeConverter() {
+      let volume = volumeForm.value
+      let count = countForm.value
+      let cost = costForm.value
+      let totalOunces = volume*count
+      let answer = cost/totalOunces
+      answerBox.innerText = 
+         `A ${count} pack of ${volume} ounce beers at a cost of $${cost} has a value of $${answer.toFixed(2)} per ounce.`
+   }
+}
